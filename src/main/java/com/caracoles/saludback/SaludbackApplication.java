9acf1e8fb309d08d5c3afa211fed162e34f2bc2d
Fihoca07 <Fihoca07@SINENSIA.EDU>
2019-12-11 09:21:42 +0100

    initial commit

---
+++ src/main/java/com/caracoles/saludback/SaludbackApplication.java
@@ -0,0 +1,13 @@
+package com.caracoles.saludback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludbackApplication.class, args);
+	}
+
+}
