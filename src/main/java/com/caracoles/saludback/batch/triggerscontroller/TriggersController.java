b059ebb6b8b4f2f98a550b428e754887d0d1c370
eduganu <eduganau@gmail.com>
2019-12-18 09:53:48 +0100

    la base de datos carga automaticamente desde el application properties

--- src/main/java/com/caracoles/saludback/batch/triggerscontroller/TriggersController.java
+++ src/main/java/com/caracoles/saludback/batch/triggerscontroller/TriggersController.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
 @RestController
 public class TriggersController {
 	
@@ -51,3 +51,4 @@ public class TriggersController {
 	
 }
 
+*/
